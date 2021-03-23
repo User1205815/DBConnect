c367e28c3b05657e3f309c6e5c4847b190768562
User1205815 <81161570+User1205815@users.noreply.github.com>
2021-03-23 12:24:28 +0100

    Delete login.sh

--- login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. /etc/openvpn/script/config.sh
-##Authentication
-user_id=$(mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -sN -e "select login from users where login = '$username' AND password = '$password' AND user_enable=1")
-##Check user
-[ "$user_id" != '' ] && [ "$user_id" = "$username" ] && echo "user : $username" && echo 'authentication ok.' && exit 0 || echo 'authentication failed.'; exit 1
