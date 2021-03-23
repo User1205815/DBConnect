8734dc92642b225d2233f2d4a59a6aa6544d878a
User1205815 <81161570+User1205815@users.noreply.github.com>
2021-03-23 12:24:24 +0100

    Delete test_connect_db.sh

--- test_connect_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /etc/openvpn/script/config.sh
-##Test Authentication
-username=$1
-password=$2
-user_id=$(mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -sN -e "select login from users where login = '$username' AND password = '$password' AND user_enable=1")
-##Check user
-[ "$user_id" != '' ] && [ "$user_id" = "$username" ] && echo "user : $username" && echo 'authentication ok.' && exit 0 || echo 'authentication failed.'; exit 1
